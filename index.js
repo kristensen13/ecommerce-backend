7adc7fad5478d00c291af68019b10ea3cc2bf6e2
kristensen13 <5943343@alu.murciaeduca.es>
2024-08-31 20:37:04 +0200

    SPA control

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require("express");
 require("dotenv").config();
+const path = require("path");
+
 const cors = require("cors");
 
 const { dbConnection } = require("./database/config");
@@ -28,6 +30,11 @@ app.use("/api/products", require("./routes/products"));
 app.use("/api/all", require("./routes/searches"));
 app.use("/api/upload", require("./routes/uploads"));
 app.use("/api/login", require("./routes/auth"));
+
+// Listen requests
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public/index.html"));
+});
 
 app.listen(process.env.PORT, () => {
   console.log("App listening on port 3000");
